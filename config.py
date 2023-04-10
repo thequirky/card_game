9c3936f798399275004767d195ab2e179d9f9202
evan <chatziva@protonmail.com>
2023-04-10 14:53:13 +0100

    changed: config class to factory function

--- config.py
+++ config.py
@@ -5,22 +5,20 @@ from scoreboard import ScoreBoard
 from ui import CLI
 
 
-class GameConfig:
-    def __init__(self, names: list[str], pile_str: str) -> None:
-        self.cli = CLI()
-        self.player1 = Player(names[0])
-        self.player2 = Player(names[1])
-        self.scoreboard = ScoreBoard()
-        self.scoreboard.register(names[0])
-        self.scoreboard.register(names[1])
-        self.game_pile = Pile.from_str(seed_str=pile_str, name="game")
-        self.discard_pile = Pile("discard")
-        self.ui = CLI()
-        self.game = CardGame(
-            player=self.player1,
-            other_player=self.player2,
-            game_pile=self.game_pile,
-            discard_pile=self.discard_pile,
-            ui=self.ui,
-            scoreboard=self.scoreboard,
+def config_game(name1: str, name2: str, pile_str: str) -> CardGame:
+        cli = CLI()
+        player1 = Player(name1)
+        player2 = Player(name2)
+        scoreboard = ScoreBoard()
+        scoreboard.register(name1)
+        scoreboard.register(name2)
+        game_pile = Pile.from_str(seed_str=pile_str, name="game")
+        discard_pile = Pile("discard")
+        return CardGame(
+            player=player1,
+            other_player=player2,
+            game_pile=game_pile,
+            discard_pile=discard_pile,
+            ui=cli,
+            scoreboard=scoreboard,
         )
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from config import GameConfig
+from config import config_game
 
 
 def main():
-    config = GameConfig(names=["Evan", "Viola"], pile_str="AKKQQQJJJJ")
-    game = config.game
+    game = config_game(name1="evan", name2="viola", pile_str="AKKQQQJJJJ")
     game.run(nb_rounds=5)
 
 
