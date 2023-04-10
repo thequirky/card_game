8c7b351c082b4c776a5f20312ffc24144a89a8a7
evan <chatziva@protonmail.com>
2023-04-10 11:59:50 +0100

    added: logging class

--- ui.py
+++ ui.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass, field
+from enum import IntEnum, auto
 from typing import Protocol
 
 from card import Pile
@@ -44,7 +46,7 @@ class CLI:
         if not player:
             msg = "\nGame is a tie...!"
         else:
-            msg = f"\n{player.name} wins the game!!!"
+            msg = f"\n!!! {player.name} wins the game !!!\n"
         print(msg)
 
     @staticmethod
@@ -68,3 +70,43 @@ class CLI:
     @staticmethod
     def render_msg(msg: str) -> None:
         print(msg)
+
+
+class LogLevels(IntEnum):
+    INFO = auto()
+    WARNING = auto()
+    ERROR = auto()
+    DEBUG = auto()
+
+
+@dataclass
+class Log:
+    level: IntEnum
+    data: list[tuple[IntEnum, str]] = field(default_factory=list)
+
+    def log(self, level: IntEnum, msg: str) -> None:
+        self.data.append((level, msg))
+        if level <= self.level:
+            print(f"{msg}")
+
+    def show_all(self) -> None:
+        for item in self.data:
+            print(item)
+
+    def __str__(self) -> str:
+        new_str = []
+        for level, msg, in self.data:
+            new_str.append(f"{str(level.name)}: {msg}")
+        return "\n".join(new_str)
+
+
+if __name__ == "__main__":
+    log_data = Log(level=LogLevels.ERROR)
+
+    log_data.log(LogLevels.INFO, msg="This is an info log msg")
+    log_data.log(LogLevels.WARNING, msg="This is a warning msg")
+    log_data.log(LogLevels.ERROR, msg="This is an error log msg")
+    log_data.log(LogLevels.DEBUG, msg="This is a debug msg")
+
+    log_data.show_all()
+    print(log_data)
