df2d9e28aa8affeb4a7168fb13cbc95b2b799c78
dingo <chatziva@protonmail.com>
2023-10-12 21:31:29 +0100

    changed game actions from class to functions

--- game.py
+++ game.py
@@ -6,35 +6,28 @@ from scoreboard import ScoreBoard
 from ui import UI
 
 
-class Actions:
-    def __init__(self, players: tuple[Player], game_pile: Pile, discard_pile: Pile, scoreboard: ScoreBoard) -> None:
-        self.game_pile = game_pile
-        self.discard_pile = discard_pile
-        self.players = players
-        self.scoreboard = scoreboard
+def deal_random(players: tuple[Player], game_pile: Pile) -> None:
+    for player in players:
+        drawn_card = game_pile.draw_random()
+        player.hold(drawn_card)
 
-    def deal_random(self) -> None:
-        for player in self.players:
-            drawn_card = self.game_pile.draw_random()
-            player.hold(drawn_card)
+def discard_all(players: tuple[Player], discard_pile: Pile) -> None:
+    for player in players:
+        discarded = player.discard()
+        discard_pile.add(discarded)
 
-    def discard_all(self) -> None:
-        for player in self.players:
-            discarded = player.discard()
-            self.discard_pile.add(discarded)
+def reshuffle(game_pile: Pile, discard_pile: Pile) -> None:
+    game_pile.reshuffle(discard_pile)
+    discard_pile.cards = []
 
-    def reshuffle(self) -> None:
-        self.game_pile.reshuffle(self.discard_pile)
-        self.discard_pile.cards = []
+def get_round_winners(players: tuple[Player]) -> tuple[Player]:
+    max_value = max(player.hand.value for player in players)
+    return tuple(player for player in players if player.hand.value == max_value)
 
-    def get_round_winners(self) -> tuple[Player]:
-        max_value = max(p.hand.value for p in self.players)
-        return tuple(p for p in self.players if p.hand.value == max_value)
-
-    def update_scoreboard(self) -> None:
-        for winner in self.get_round_winners():
-            self.scoreboard.actions.increment_score_of(name=winner.name, value=winner.hand.value)
-            self.scoreboard.actions.increment_rounds_of(winner.name)
+def update_scoreboard(scoreboard: ScoreBoard, players: tuple[Player]) -> None:
+    for winner in get_round_winners(players):
+        scoreboard.actions.increment_score_of(name=winner.name, value=winner.hand.value)
+        scoreboard.actions.increment_rounds_of(winner.name)
 
 
 class CardGame:
@@ -44,21 +37,15 @@ class CardGame:
         self.ui = ui
         self.scoreboard = scoreboard
         self.discard_pile = Pile("discard")
-        self.actions = Actions(
-            players=self.players, 
-            game_pile=self.game_pile, 
-            discard_pile=self.discard_pile, 
-            scoreboard=self.scoreboard,
-        )
 
     def do_round(self) -> None:
         self.game_pile.shuffle()
         self.ui.render_pile(self.game_pile)
-        self.actions.deal_random()
-        self.actions.update_scoreboard()
+        deal_random(self.players, self.game_pile)
+        update_scoreboard(self.scoreboard, self.players)
         self.ui.render_hands(self.players)
-        self.ui.render_round_winner(self.actions.get_round_winners())
-        self.actions.discard_all()
+        self.ui.render_round_winner(get_round_winners(self.players))
+        discard_all(self.players, self.discard_pile)
         # self.ui.render_pile(self.game_pile)
         # self.ui.render_pile(self.discard_pile)
         self.ui.render_scoreboard(str(self.scoreboard))
@@ -75,11 +62,11 @@ class CardGame:
 
             if self.game_pile.is_empty():
                 logging.info("No more cards left to pick from -> reshuffling")
-                self.actions.reshuffle()
+                reshuffle(self.game_pile, self.discard_pile)
 
             elif more_players_than_cards_left:
                 logging.info("Not enough cards in the pile for all players -> reshuffling")
-                self.actions.reshuffle()
+                reshuffle(self.game_pile, self.discard_pile)
 
             self.do_round()
 
@@ -94,7 +81,7 @@ if __name__ == "__main__":
 
     logging.basicConfig(level=logging.WARNING)
 
-    names = ("evan", "viola", "lenka")
+    names = ("viola", "evan", "lenka")
     names = deduplicate(names)
     players = Player.from_names(names)
     scoreboard = ScoreBoard.from_players(players)
